4faca505fc340a571dde1567366096137e8763f6
Siddarthapogula <pogulasiddharth96@gmail.com>
2023-12-01 01:13:48 +0530

    learnt components and elements

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
+import  ReactDOM  from "react-dom/client";
 
-const heading = React.createElement("h1", {}, [
-    "this is heading",
-]);
+
+
+const Heading1 = ()=>{
+    return(
+        <div>
+        <   h1>this is heading no 1 🚀</h1>
+        </div>
+       
+    )
+}
+
+const headElement = (
+        <div>
+            <Heading1/>
+            <h1>this is heading in element 🚀🚩</h1>
+        </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(headElement);
