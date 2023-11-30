1cc462a383111b7a8928b5ba804600930cb89cac
Siddarthapogula <pogulasiddharth96@gmail.com>
2023-11-30 15:59:09 +0530

    deleted layout component i am currently learning how to use git that is why am doing commit frequently

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
-import Layout from "./Layout";
 
 const heading = React.createElement("h1", {}, [
     "this is heading",
-    <Layout/>
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- Layout.js
+++
@@ -1,6 +0,0 @@
-const Layout = ()=>{
-    return(
-        <h1>this is layout</h1>
-    )
-}
-export default Layout;
