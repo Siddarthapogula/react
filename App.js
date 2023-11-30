ea1a05834eceae1a8aa3e044857fdb5cc8bde8c8
Siddarthapogula <pogulasiddharth96@gmail.com>
2023-11-30 15:30:16 +0530

    added a layout component

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
+import Layout from "./Layout";
 
-const heading = React.createElement("h1", {}, "this is heading");
+const heading = React.createElement("h1", {}, [
+    "this is heading",
+    <Layout/>
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(heading);
---
+++ Layout.js
@@ -0,0 +1,6 @@
+const Layout = ()=>{
+    return(
+        <h1>this is layout</h1>
+    )
+}
+export default Layout;
