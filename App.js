77c8fb567db85a656a41e4048dcdf79e4857cebd
Siddarthapogula <pogulasiddharth96@gmail.com>
2023-12-01 16:38:03 +0530

    learnt about props

--- App.js
+++ App.js
@@ -1,24 +1,38 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
+const element1 = ( 
+    <h1>this is element1</h1>
+);
+const element2 = ( 
+    <h1>this is element2</h1>
+);
+const element3 = ( 
+    <h1>this is element3</h1>
+);
+const element4 = ( 
+    <h1>this is element4</h1>
+);
 
+const elementArray = [element1, element2, element3, element4];
 
-const Heading1 = ()=>{
+const ExecuteElement = function (props){
+    console.log(props.content);
+}
+
+const ElementPass = ()=>{
     return(
-        <div>
-        <   h1>this is heading no 1 🚀</h1>
-        </div>
-       
+        <>
+           { elementArray.map((element)=>{
+                <ExecuteElement content= {element.props.children}/>
+            })}
+        </>
     )
 }
 
-const headElement = (
-        <div>
-            <Heading1/>
-            <h1>this is heading in element 🚀🚩</h1>
-        </div>
-)
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(headElement);
+root.render(< ElementPass />);
+
--- README.md
+++ README.md
@@ -50,3 +50,52 @@
 
 ## see you repo
 
+
+## Episode 4
+
+# code : 
+
+- talk is cheap! show me the code :)
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const element1 = ( 
+    <h1>this is element1</h1>
+);
+const element2 = ( 
+    <h1>this is element2</h1>
+);
+const element3 = ( 
+    <h1>this is element3</h1>
+);
+const element4 = ( 
+    <h1>this is element4</h1>
+);
+
+const elementArray = [element1, element2, element3, element4];
+
+const ExecuteElement = function (props){
+    console.log(props.content);
+}
+
+const ElementPass = ()=>{
+    return(
+        <>
+           { elementArray.map((element)=>{
+                <ExecuteElement content= {element.props.children}/>
+            })}
+        </>
+    )
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(< ElementPass />);
+
+
+# started building a foodOrdering app
+
